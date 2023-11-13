cf87b6238e0d6d3d0f205c3f8d6022438b4b835d
Anggunksm <202010215220@mhs.ubharajaya.ac.id>
2023-11-13 20:41:30 +0700

    update project structure

---
+++ src/test/java/starter/utils/JsonSchema.java
@@ -0,0 +1,22 @@
+package starter.utils;
+
+public class JsonSchema {
+    public static final String USER_LOGIN_RESPONSE_SCHEMA = "schema/user-login-response-schema.json";
+    public static final String HELLO_RESPONSE_SCHEMA = "schema/hello-response-schema.json";
+    public static final String REGISTER_RESPONSE_SCHEMA = "schema/register-response-schema.json";
+    public static final String GET_ALL_PRODUCTS_RESPONSE_SCHEMA = "schema/get-all-products-response-schema.json";
+    public static final String GET_ALL_CATEGORIES_RESPONSE_SCHEMA = "schema/get-all-categories-response-schema.json";
+    public static final String GET_ALL_ORDERS_RESPONSE_SCHEMA = "schema/get-all-orders-response-schema.json";
+    public static final String GET_SINGLE_PRODUCT_RESPONSE_SCHEMA ="schema/get-single-product-response-schema.json";
+    public static final String GET_SINGLE_CATEGORY_RESPONSE_SCHEMA = "schema/get-single-category-response-schema.json";
+    public static final String GET_PRODUCT_RATINGS_RESPONSE_SCHEMA = "schema/get-product-ratings-response-schema.json";
+    public static final String GET_PRODUCT_COMMENTS_RESPONSE_SCHEMA = "schema/get-product-comments-response-schema.json";
+    public static final String DELETE_SINGLE_PRODUCT_RESPONSE_SCHEMA = "schema/schema/delete-single-product-response-schema.json";
+    public static final String CREATE_ORDER_RESPONSE_SCHEMA = "schema/create-order-response-schema.json";
+    public static final String CREATE_PRODUCT_RESPONSE_SCHEMA = "schema/create-product-response-schema.json";
+    public static final String GET_SINGLE_ORDER_RESPONSE_SCHEMA = "schema/get-single-order-response-schema.json";
+    public static final String GET_USER_INFORMATION_RESPONSE_SCHEMA = "schema/get-user-information-response-schema.json";
+    public static final String ASSIGN_PRODUCT_RATINGS_RESPONSE_SCHEMA ="schema/assign-product-ratings-response-schema.json";
+    public static final String CREATE_PRODUCT_COMMENT_RESPONSE_SCHEMA = "schema/create-product-comment-response-schema.json";
+
+}
