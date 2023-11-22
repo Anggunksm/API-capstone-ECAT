385be2379c4f567083f7c3c00e8dc6fb967c837f
UmiUswatun <142196617+UmiUswatun@users.noreply.github.com>
2023-11-22 19:15:34 +0700

    added register

--- src/test/java/starter/utils/JsonSchema.java
+++ src/test/java/starter/utils/JsonSchema.java
@@ -3,7 +3,7 @@ package starter.utils;
 public class JsonSchema {
     public static final String USER_LOGIN_RESPONSE_SCHEMA = "schema/user-login-response-schema.json";
     public static final String HELLO_RESPONSE_SCHEMA = "schema/hello-response-schema.json";
-    public static final String REGISTER_RESPONSE_SCHEMA = "schema/register-response-schema.json";
+    public static final String REGISTER_RESPONSE_SCHEMA = "schema/user/register-response-schema.json";
     public static final String GET_ALL_PRODUCTS_RESPONSE_SCHEMA = "schema/get-all-products-response-schema.json";
     public static final String GET_ALL_CATEGORIES_RESPONSE_SCHEMA = "schema/get-all-categories-response-schema.json";
     public static final String GET_ALL_ORDERS_RESPONSE_SCHEMA = "schema/get-all-orders-response-schema.json";
